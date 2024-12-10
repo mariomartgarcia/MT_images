63491db1299afcdee041681ff49a044d56bc4ec4
Mario Martínez <mmartinez@bcamath.org>
2024-12-10 16:41:05 -0500

    MT for image first full code

--- MT.py
+++ MT.py
@@ -110,6 +110,7 @@ def create_simple_classifier(input_shape=(128, 64, 3)):
 
     return models.Model(inputs=inputs, outputs=classification_output)
 
+
 # %%
 
 from tensorflow.keras import layers, models
@@ -182,28 +183,6 @@ predictions = np.round(np.ravel(multi_task_model.predict(test_rgb)['classificati
 # Calculate errors
 print(1 - accuracy_score(test_labels, predictions))
 
-# %%
-
-
-
-
-'''
-def multi_task(y_true, y_pred):
-    y_tr = tf.reshape(y_true[:, 0], [-1,1])
-    pri = tf.reshape(y_true[:, 1], [-1,1])
-
-
-    #print(y_pred)
-    c_pre, pi_pre, sigma_c, sigma_T  = tf.unstack(y_pred, num=2, axis=1)
-    c_pre = tf.reshape(c_pre, [-1, 1]) 
-    pi_pre = tf.reshape(pi_pre, [-1, 1]) 
-    #tf.print(c_pre)
-
-    l1 = (1/(2*tf.math.exp(sigma_c)))*tf.keras.losses.mean_squared_error(pri, pi_pre) + tf.math.log(tf.sqrt(tf.math.exp(sigma_c)))
-    l2 = (1/(tf.math.exp(sigma_T)))*tf.keras.losses.binary_crossentropy(y_tr, c_pre) + tf.math.log(tf.sqrt(tf.math.exp(sigma_T)))
-    return tf.reduce_mean(l1 + l2)
-'''
-
 
 
 # %%
@@ -270,4 +249,123 @@ predictions = np.round(np.max(pred[:,:,:,1], axis = (1,2)))
 # Calculate errors
 print(1 - accuracy_score(test_labels, predictions))
 
+
+
+# %%
+
+#MULTI-TASK APPROACH WITH SIGMA AND TEMPERATURE
+
+
+class ExtLayer(layers.Layer):
+        def __init__(self, initial_sigma=0.1):
+            super(ExtLayer, self).__init__()
+            # Initialize the trainable variable
+            self.var = tf.Variable(initial_value=[[initial_sigma]], dtype=tf.float32, trainable=True)
+
+        def call(self, inputs):
+            batch_size = tf.shape(inputs)[0]  # Get the current batch size
+            # Tile sigma to match the batch size
+            tensor = tf.tile(self.var, [batch_size, 1])
+            return tensor
+
+
+
+# Crear modelos individuales
+unet_model = simple_unet(input_shape=(64, 64, 3))
+classifier_model = create_simple_classifier(input_shape=(128, 64, 3))  # Concatenated RGB + IR
+
+# Entrada principal (imagen RGB)
+rgb_input = layers.Input(shape=(64, 64, 3))
+
+# Salida de la U-Net (IR predicha)
+ir_predicted = unet_model(rgb_input)  # Salida (64, 64, 1)
+
+# Concatenar RGB con IR predicha en el eje vertical (espacial)
+# Expandir el IR predicho a 3 canales para que coincida con RGB
+ir_predicted_3c = layers.Concatenate(axis=-1)([ir_predicted] * 3)  # (64, 64, 3)
+
+# Concatenar RGB e IR verticalmente
+concatenated = layers.Concatenate(axis=1)([rgb_input, ir_predicted_3c])  # Salida (128, 64, 3)
+
+# Clasificación binaria
+classification_output = classifier_model(concatenated)
+
+
+classification_output_expanded = layers.Reshape((1, 1, 1))(classification_output)
+classification_output_upsampled = layers.UpSampling2D(size=(64, 64))(classification_output_expanded) 
+
+
+#External parameters
+sigma_output = ExtLayer()(rgb_input)
+temp_output = ExtLayer()(rgb_input)
+
+
+sigma_out_expand = layers.Reshape((1, 1, 1))(sigma_output)
+sigma = layers.UpSampling2D(size=(64, 64))(sigma_out_expand) 
+
+temperature_out_expand = layers.Reshape((1, 1, 1))(temp_output)
+temperature = layers.UpSampling2D(size=(64, 64))(temperature_out_expand)
+
+
+conc= layers.Concatenate(axis=-1)([ir_predicted, classification_output_upsampled,
+                                    sigma, temperature])
+
+
+multi_task_model = models.Model(
+    inputs=rgb_input, 
+    outputs=[conc]
+)
+
+# %%
+
+
+def multi_task(y_true, y_pred):
+    pri = y_true[:, :, :, 0]
+    y_tr = tf.reshape(tf.reduce_max(y_true[:, :, :, 1], axis=(1, 2)), [-1, 1])
+
+    pi_pre = y_pred[:, :, :, 0]
+    c_pre = tf.reshape(tf.reduce_max(y_pred[:, :, :, 1], axis=(1, 2)), [-1, 1])
+    sigma = tf.reshape(tf.reduce_max(y_pred[:, :, :, 2], axis=(1, 2)), [-1, 1])
+    temperature = tf.reshape(tf.reduce_max(y_pred[:, :, :, 3], axis=(1, 2)), [-1, 1])
+
+    l1 = (1/(2*tf.math.exp(sigma)))*tf.reduce_mean(tf.square(pi_pre - pri), axis=[1, 2]) + tf.math.log(tf.sqrt(tf.math.exp(sigma)))
+    l2 = (1/(tf.math.exp(temperature)))*tf.keras.losses.binary_crossentropy(y_tr, c_pre)  + tf.math.log(tf.sqrt(tf.math.exp(temperature)))
+
+    return tf.reduce_mean(l1 + l2)
+
+
+
+multi_task_model.compile(
+    optimizer='adam',
+    loss=multi_task
+)
+
+
+
+expanded_array = np.expand_dims(train_labels, axis=(-1, -2, -3))  # Forma [n, 1, 1, 1]
+train_label_ex = np.tile(expanded_array, (1, 64, 64, 1)) 
+
+
+expanded_array = np.expand_dims(val_labels, axis=(-1, -2, -3))  # Forma [n, 1, 1, 1]
+val_label_ex = np.tile(expanded_array, (1, 64, 64, 1)) 
+
+y_MT = np.concatenate([train_nir, train_label_ex], axis = -1)
+y_MT_val = np.concatenate([val_nir, val_label_ex], axis = -1)
+
+
+
+# Entrenamiento
+history = multi_task_model.fit(
+    train_rgb, y_MT,
+    validation_data=(val_rgb, y_MT_val),
+    epochs=1,
+    batch_size=500,
+)
+
+# %%
+
+pred = multi_task_model.predict(test_rgb)
+predictions = np.round(np.max(pred[:,:,:,1], axis = (1,2)))
+# Calculate errors
+print(1 - accuracy_score(test_labels, predictions))
 # %%
--- utils.py
+++ utils.py
@@ -2,6 +2,11 @@ import os
 import numpy as np
 from PIL import Image
 import tifffile as tiff
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+import tensorflow as tf
+
+def LUPI_gain(ub, lb, x):
+    return ((x - lb) / (ub - lb) )*100
 
 def extract_and_save_rgb_ir_eurosat(input_folder):
     """
@@ -65,3 +70,134 @@ def extract_and_save_rgb_ir_eurosat(input_folder):
                 print(f"Saltando archivo (estructura no válida o no tiene suficientes bandas): {file_path}")
 
     print("Proceso completado.")
+
+
+
+
+def load_image_pairs_with_labels(data_dir, classes, input_size=(64, 64)):
+    rgb_paths = []
+    nir_paths = []
+    labels = []  # Guardar etiquetas: 0 para 'Highway', 1 para 'River'
+
+    for label, class_name in enumerate(classes):
+        rgb_folder = os.path.join(data_dir, f'{class_name}RGB')
+        nir_folder = os.path.join(data_dir, f'{class_name}NIR')
+
+        for file_name in os.listdir(rgb_folder):
+            base_name = "_".join(file_name.split('_')[:-1]) 
+
+            rgb_path = os.path.join(rgb_folder, base_name + '_RGB.png')
+            nir_path = os.path.join(nir_folder, base_name + '_NIR.png')
+
+            if os.path.exists(rgb_path) and os.path.exists(nir_path):
+                rgb_paths.append(rgb_path)
+                nir_paths.append(nir_path)
+                labels.append(label)  # Etiqueta basada en el índice de `classes`
+            else:
+                print('Warning mismatch')
+                print(rgb_path)
+                print(nir_path)
+
+    # Load and preprocess images
+    def preprocess_image(image_path):
+        img = load_img(image_path, target_size=input_size)
+        return img_to_array(img) / 255.0  # Normalize to [0, 1]
+
+    rgb_images = np.array([preprocess_image(path) for path in rgb_paths])
+    nir_images = np.array([preprocess_image(path)[:, :, 0:1] for path in nir_paths])  # Grayscale NIR
+    labels = np.array(labels)  # Convertir las etiquetas a numpy array
+    
+    return rgb_images, nir_images, labels
+
+
+
+def concatenate_images(rgb_images, nir_predictions):
+    expand = np.repeat(nir_predictions, 3, axis=-1)
+    pre_pri = np.concatenate([rgb_images, expand], axis=1)
+    return pre_pri  
+
+def expand_array(arr, dim = (1, 64, 64, 1)):
+    expanded_array = np.expand_dims(arr, axis=(-1, -2, -3))  
+    ex = np.tile(expanded_array, dim) 
+    return ex
+
+def loss_TPD(T, beta, l):
+    def loss(y_true, y_pred):
+        y_tr = y_true[:, 0]
+        y_prob = y_true[:, 1]
+        d = y_true[:, 2]
+        
+        ft = (-tf.math.log(1/(y_prob+1e-6) - 1 + 1e-6)) / T
+        y_pr = 1 / (1 + tf.exp(-ft))
+
+        bce = tf.keras.losses.BinaryCrossentropy(reduction=tf.keras.losses.Reduction.NONE)
+        bce_inst = bce(y_pred, y_pr )
+        bce_r = tf.keras.losses.BinaryCrossentropy(reduction=tf.keras.losses.Reduction.NONE)(y_tr, y_pred)
+        return tf.reduce_mean((1-l)*(bce_r) + l*(tf.math.multiply(d,bce_inst) - beta * tf.math.multiply(1-d, bce_inst))) 
+    return loss
+
+def loss_GD(T, l):
+    def loss(y_true, y_pred):
+        y_tr = y_true[:, 0]
+        y_prob = y_true[:, 1]
+
+        ft = (-tf.math.log(1/(y_prob+1e-6) - 1 + 1e-6)) / T
+        y_pr = 1 / (1 + tf.exp(-ft))
+        d1 = tf.keras.losses.BinaryCrossentropy(reduction=tf.keras.losses.Reduction.NONE)(y_tr, y_pred)
+        d2 = tf.keras.losses.BinaryCrossentropy(reduction=tf.keras.losses.Reduction.NONE)(y_pr, y_pred)
+        return tf.reduce_mean( (1-l)*d1 + l*d2)
+    return loss
+
+def loss_MT(y_true, y_pred):
+    pri = y_true[:, :, :, 0]
+    y_tr = tf.reshape(tf.reduce_max(y_true[:, :, :, 1], axis=(1, 2)), [-1, 1])
+
+    pi_pre = y_pred[:, :, :, 0]
+    c_pre = tf.reshape(tf.reduce_max(y_pred[:, :, :, 1], axis=(1, 2)), [-1, 1])
+    sigma = tf.reshape(tf.reduce_max(y_pred[:, :, :, 2], axis=(1, 2)), [-1, 1])
+    temperature = tf.reshape(tf.reduce_max(y_pred[:, :, :, 3], axis=(1, 2)), [-1, 1])
+
+    l1 = (1/(2*tf.math.exp(sigma)))*tf.reduce_mean(tf.square(pi_pre - pri), axis=[1, 2]) + tf.math.log(tf.sqrt(tf.math.exp(sigma)))
+    l2 = (1/(tf.math.exp(temperature)))*tf.keras.losses.binary_crossentropy(y_tr, c_pre)  + tf.math.log(tf.sqrt(tf.math.exp(temperature)))
+
+    return tf.reduce_mean(l1 + l2)
+
+
+
+def loss_MT_TPD(y_true, y_pred):
+    pri = y_true[:, :, :, 0]
+    y_tr = tf.reshape(tf.reduce_max(y_true[:, :, :, 1], axis=(1, 2)), [-1, 1])
+    y_upper = tf.reshape(tf.reduce_max(y_true[:, :, :, 2], axis=(1, 2)), [-1, 1])
+    d = tf.reshape(tf.reduce_max(y_true[:, :, :, 3], axis=(1, 2)), [-1, 1])
+
+
+
+    pi_pre = y_pred[:, :, :, 0]
+    c_pre = tf.reshape(tf.reduce_max(y_pred[:, :, :, 1], axis=(1, 2)), [-1, 1])
+    sigma = tf.reshape(tf.reduce_max(y_pred[:, :, :, 2], axis=(1, 2)), [-1, 1])
+    temperature = tf.reshape(tf.reduce_max(y_pred[:, :, :, 3], axis=(1, 2)), [-1, 1])
+
+    l1 = (1/(2*tf.math.exp(sigma)))*tf.reduce_mean(tf.square(pi_pre - pri), axis=[1, 2]) + tf.math.log(tf.sqrt(tf.math.exp(sigma)))
+    bce_r = tf.keras.losses.binary_crossentropy(y_tr, c_pre)
+    bce_inst = tf.keras.losses.binary_crossentropy(c_pre, y_upper) 
+    l2 = (1/(tf.math.exp(temperature)))*(0.5*(bce_r) + 0.5*(tf.math.multiply(d,bce_inst) - tf.math.multiply(1-d, bce_inst))) + tf.math.log(tf.sqrt(tf.math.exp(temperature)))
+
+    return tf.reduce_mean(l1 + l2)
+
+
+
+def loss_MT_PFD(y_true, y_pred):
+    pri = y_true[:, :, :, 0]
+    y_tr = tf.reshape(tf.reduce_max(y_true[:, :, :, 1], axis=(1, 2)), [-1, 1])
+    y_upper = tf.reshape(tf.reduce_max(y_true[:, :, :, 2], axis=(1, 2)), [-1, 1])
+
+    pi_pre = y_pred[:, :, :, 0]
+    c_pre = tf.reshape(tf.reduce_max(y_pred[:, :, :, 1], axis=(1, 2)), [-1, 1])
+    sigma = tf.reshape(tf.reduce_max(y_pred[:, :, :, 2], axis=(1, 2)), [-1, 1])
+    temperature = tf.reshape(tf.reduce_max(y_pred[:, :, :, 3], axis=(1, 2)), [-1, 1])
+
+    l1 = (1/(2*tf.math.exp(sigma)))*tf.reduce_mean(tf.square(pi_pre - pri), axis=[1, 2]) + tf.math.log(tf.sqrt(tf.math.exp(sigma)))
+    bce = 0.5*tf.keras.losses.binary_crossentropy(y_upper, c_pre) + 0.5*tf.keras.losses.binary_crossentropy(y_tr, c_pre) 
+    l2 = (1/(tf.math.exp(temperature)))*(bce) + tf.math.log(tf.sqrt(tf.math.exp(temperature)))
+
+    return tf.reduce_mean(l1 + l2)
